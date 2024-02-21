1bb54a0bb3c24c097f12221323597223b88aaf44
Oussama Aouina <oussamaaouina99@gmail.com>
2024-02-21 23:05:29 +0100

    add kafkaservice to consume the messages

--- src/main/java/com/aop/query/service/KafkaService.java
+++ src/main/java/com/aop/query/service/KafkaService.java
@@ -1,2 +1,34 @@
-package com.aop.query.service;public class KafkaService {
+package com.aop.query.service;
+
+import com.aop.query.model.Product;
+import com.aop.query.repository.ProductRepo;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import static java.lang.String.format;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class KafkaService {
+    @Autowired
+    private ProductRepo productRepo;
+
+//    @KafkaListener(topics ="stock",groupId ="sells")
+//    public void consumeMessage(String msg){
+//        log.info(format("the consumed message is::%s",msg));
+//    }
+
+    @KafkaListener(topics = "stock",groupId = "sells")
+    public void consumeMessage(Product product){
+        productRepo.save(product);
+        log.info(format("product: %s is saved successfully"));
+    }
+
 }
+
