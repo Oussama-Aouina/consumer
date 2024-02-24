fbff6e8fb693ac5461b3c8612a67e2bfd6bbc61d
Oussama Aouina <oussamaaouina99@gmail.com>
2024-02-24 23:59:07 +0100

    fixing the error of the JSON deserialization by customising a deserializer like "json parser"

--- src/main/java/com/aop/query/service/KafkaService.java
+++ src/main/java/com/aop/query/service/KafkaService.java
@@ -2,14 +2,13 @@ package com.aop.query.service;
 
 import com.aop.query.model.Product;
 import com.aop.query.repository.ProductRepo;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
-
 import static java.lang.String.format;
 
 @Service
@@ -19,16 +18,29 @@ public class KafkaService {
     @Autowired
     private ProductRepo productRepo;
 
-//    @KafkaListener(topics ="stock",groupId ="sells")
-//    public void consumeMessage(String msg){
-//        log.info(format("the consumed message is::%s",msg));
-//    }
+    @KafkaListener(topics ="stock",groupId ="sells")
+    public void consumeMessage(String msg){
+        log.info(format("the consumed message is::%s",msg));
 
-    @KafkaListener(topics = "stock",groupId = "sells")
-    public void consumeMessage(Product product){
-        productRepo.save(product);
-        log.info(format("product: %s is saved successfully"));
+        //this code is to deserilize the json object "Product" received/consumed as a string
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            JsonParser parser = mapper.createParser(msg);
+            Product product =  parser.readValueAs(Product.class);
+            log.info(format("json product: %s",product));
+            productRepo.save(product);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
     }
+
+//    @KafkaListener(topics = "stock",groupId = "sells")
+//    public void consumeMessage(Product product){
+//        productRepo.save(product);
+//        log.info(format("product: %s is saved successfully"));
+//    }
 
 }
 
